0f6d7db73196aaf7849dd729cae83848f171d0c5
yiqingzhou1 <yqzhou1997@gmail.com>
2021-01-02 22:20:52 -0500

    The basic version

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class UserItem extends Component {
+    state = {
+        id: 'id',
+        login: 'mojombo',
+        avatar_url: 'http://touxiangkong.com/touxiang/21631.html',
+        html_url: '#'
+    };
+
+    render() {
+
+        const { login, avatar_url, html_url} = this.state;
+        return (
+            <div className="card text-center">
+                <img src={avatar_url} alt="" className="round-img" style={{ width: '60px'}} />
+                <h3>{login}</h3>
+                <div>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default UserItem;
